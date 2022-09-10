b6431167a5f24ef724ea01e9cb31f416daf5de10
Thiago Braga <thiago.braga.tpb@gmail.com>
2022-09-10 15:23:16 -0300

    #

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const getApi = async () => {
     const response = await fetch(api)
     const data = await response.json()
     for ( let i = 0; i < data.teste.length; i++){
-        console.log(data.teste[i].chave)
+        $root.innerHTML = data.teste[i].chave
     }
     return data
     }
