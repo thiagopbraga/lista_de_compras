22136514039c196f24a1d52d26745eb07871a653
Thiago Braga <thiago.braga.tpb@gmail.com>
2022-09-12 14:04:28 -0300

    estruturando o inicio de algo

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const getApi = async () => {
     const response = await fetch(api)
     const data = await response.json()
     for ( let i = 0; i < data.teste.length; i++){
-        $root.innerHTML = data.teste[i].chave
+        //$root.innerHTML = data.teste[i].chave
     }
     return data
     }
