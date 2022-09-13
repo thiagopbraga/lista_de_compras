1b85615e38c42ddec3d2056ebc03d7ae1bc5e9b6
Thiago Braga <thiago.braga.tpb@gmail.com>
2022-09-12 23:10:50 -0300

    Ajuste para inserir items na lista

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
-const api = "api.json"
+const newOneOnList = document.querySelector('#newOneOnList');
+const lista = document.querySelector('#list');
+const valorTotal = document.querySelector('.valorTotal');
 
-const $root = document.getElementById("root")
+/* adicionar item na lista */
+const addItem = () => {
+    const item = document.createElement('div');
+    item.classList.add('rowDivList');
+    item.innerHTML = `
+        <div class="itemListName">
+            ${newOneOnList.value}
+        </div>
+        <input type="number" placeholder="Valor..." class="itemListValue">
+        <div class="itemListRemove">
+            <button class="removeItem">Remover</button>
+        </div>
+    `;
+    lista.appendChild(item);
+    newOneOnList.value = null;
+}
 
-const getApi = async () => {
-    const response = await fetch(api)
-    const data = await response.json()
-    for ( let i = 0; i < data.teste.length; i++){
-        //$root.innerHTML = data.teste[i].chave
+const rowDivList = document.querySelectorAll('.rowDivList');
+const itemListValue = document.querySelectorAll('.itemListValue');
+
+/* calcular valor total */
+const calcValorTotal = () => {
+    let total = 0;
+    for (let i = 0; i < document.querySelectorAll('.itemListValue').length; i++) {
+        total += Number(document.querySelectorAll('.itemListValue')[i].value);
     }
-    return data
-    }
-getApi()
+    valorTotal.innerHTML = total;
+    return total;
+}
+
+setTimeout(() => {
+    calcValorTotal();
+}, 1000);
